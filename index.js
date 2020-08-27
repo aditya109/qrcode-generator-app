4195cf123f7ef253b505b4106fbe83d51ce53e63
aditya109 <adikid1996@gmail.com>
2020-08-27 09:04:07 +0530

    deploying to git
    Signed-off-by: aditya109 <adikid1996@gmail.com>

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const qrcode = require("qrcode");
 const fs = require("fs");
 const bodyParser = require("body-parser");
@@ -7,38 +7,42 @@ const bodyParser = require("body-parser");
 const app = express();
 
 async function convertToQRCode(message, response) {
-    const result = await qrcode.toDataURL(message);
-    fs.writeFileSync("./data/qr.html", `<img src="${result}">`);
-    response.send(result);
+	const result = await qrcode.toDataURL(message);
+	fs.writeFileSync("./data/qr.html", `<img src="${result}">`);
+	response.send(result);
 }
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({extended: false}));
-// Serve the static files from the React app
-app.use(express.static(path.join(__dirname, 'client')));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.post("/text", (request, response) => {
+	let message = request.query.message;
+	convertToQRCode(message, response).catch((error) =>
+		console.error(error.stack)
+	);
 
-    let message = request.query.message;
-    convertToQRCode(message, response).catch((error) => console.error(error.stack));
-
-    // response.send("200 !")
+	// response.send("200 !")
 });
 
 app.get("/download/qrCode", (request, response) => {
-    response.download(path.join(__dirname, "data", "qr.html"));
-})
+	response.download(path.join(__dirname, "data", "qr.html"));
+});
 
-app.get('/ttest', (req, res) => {
-    res.send("200 !")
-})
-
-// Handles any requests that don't match the ones above
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/client/public/index.html'));
+app.get("/ttest", (req, res) => {
+	res.send("200 !");
 });
 
 const port = process.env.PORT || 5000;
+
+if (process.env.NODE__ENV === "production") {
+	// Serve the static files from the React app
+	app.use(express.static(path.join(__dirname, "client")));
+	// Handles any requests that don't match the ones above
+	app.get("*", (req, res) => {
+		res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+	});
+}
+
 app.listen(port, () => {
-    console.log(`App is listening on ${port}`);
+	console.log(`App is listening on ${port}`);
 });
