161430857becd24b5fac0aa219dfc8de24b62820
aditya109 <adikid1996@gmail.com>
2020-08-27 13:24:22 +0530

    deploying to keroku
    Signed-off-by: aditya109 <adikid1996@gmail.com>

--- index.js
+++ index.js
@@ -4,8 +4,11 @@ const qrcode = require("qrcode");
 const fs = require("fs");
 const bodyParser = require("body-parser");
 
+
 const app = express();
-const port = process.env.PORT || 5000;
+const dev = app.get("env") !== "production";
+app.use(bodyParser.urlencoded({ extended: false }));
+
 
 async function convertToQRCode(message, response) {
 	const result = await qrcode.toDataURL(message);
@@ -13,16 +16,13 @@ async function convertToQRCode(message, response) {
 	response.send(result);
 }
 
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
-
 // OK
 app.post("/api/text", (request, response) => {
 	let message = request.query.message;
 	convertToQRCode(message, response).catch((error) =>
-	console.error(error.stack)
+		console.error(error.stack)
 	);
-	
+
 	// response.send("200 !")
 });
 
@@ -31,13 +31,27 @@ app.get("/api/download/qrCode", (request, response) => {
 	response.download(path.join(__dirname, "data", "qr.html"));
 });
 
-// Serve the static files from the React app
-app.use(express.static(path.join(__dirname, "client", "build")));
-// Handles any requests that don't match the ones above
-app.get("/", (req, res) => {
-	res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-});
+if (!dev) {
+	app.disable("x-powered-by");
+	// Serve the static files from the React app
+	app.use(express.static('client/build'));
+	// Handles any requests that don't match the ones above
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+	});
+} 
+else {
+	// app.disable("x-powered-by");
+	// Serve the static files from the React app
+	app.use(express.static(path.join(__dirname, "client", "build")));
+	// Handles any requests that don't match the ones above
+	app.get("*", (req, res) => {
+		res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+	});
+}
+const normalizePort = port => parseInt(port, 10);
+const PORT = normalizePort(process.env.PORT || 5000)
 
-app.listen(port, () => {
-	console.log(`App is listening on ${port}`);
+app.listen(PORT, () => {
+	console.log(`App is listening on ${PORT}`);
 });
